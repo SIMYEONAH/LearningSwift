bca67bf248cd61d80984409b9faa8e9963b29b69
Yeonah Shim <simya0528@naver.com>
2025-01-09 00:42:35 +0900

    타입추론, 문자열 보간법 그리고 공백을 제외하고 글자수만 제외

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -15,5 +15,23 @@ print(anoatherPharase)
 anoatherPharase = "The quck brown fox jumps over the laze"
 print(anoatherPharase)
 
+//타입 추론, 문자열 보간법
 
+var phraseInfo = "The phrase" + " has: "
+//phraseInfo = 1 타입 추론(문자열)으로 생성되었기 때문에, 다른 타입(정수형)의 값을 가질 수 없다.
+
+phraseInfo += "\(numberOfFoxes) foxes" // 다음과 동일함: phraseInfo = phraseInfo + "\(numberOfFoxes) foxes"
+
+var i = 1
+i += 1
+print(i)
+
+//공백을 포함한 텍스트 글자 수
+print("Number of characters:  \(phraseInfo.count)")
+
+let text: String = "Hello World"
+print(text.count)
+
+let nonWhitespaceCount = text.filter { !$0.isWhitespace }.count
+print(nonWhitespaceCount) // 결과: 10 (공백 제외)
 
