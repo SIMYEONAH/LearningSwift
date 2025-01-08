20451c7ebfb694437b440202a5c3b0c2cc3a3369
Yeonah Shim <simya0528@naver.com>
2025-01-09 00:22:34 +0900

    var 변수

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -8,3 +8,12 @@ let numberOfAnimals: Int = 2
 let avarageCharactersPerWord: Float = (3+5+5+5+5+4+3+4+3) / 9
 print(avarageCharactersPerWord)
 print(pharase)
+
+//변수
+var anoatherPharase: String = "phrase"
+print(anoatherPharase)
+anoatherPharase = "The quck brown fox jumps over the laze"
+print(anoatherPharase)
+
+
+
