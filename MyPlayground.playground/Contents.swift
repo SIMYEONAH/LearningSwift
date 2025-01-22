ca70c2fbe8a1b8ff7d2f19defe073eabdfc5c49d
Yeonah Shim <simya0528@naver.com>
2025-01-22 21:03:40 +0900

    사분면 고르기 문제

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,28 +1,21 @@
 import Foundation
 
 //runFile()
+/ 사분면 구하기
+// x좌표 입력 받기
+let x = Int(readLine()!)!
 
-enum PoolBallType {
-    case solid
-    case stripe
-    case black
+// y좌표 입력 받기
+let y = Int(readLine()!)!
+
+// 사분면 판단 및 출력
+if x > 0 && y > 0 {
+    print(1) // 제1사분면
+} else if x < 0 && y > 0 {
+    print(2) // 제2사분면
+} else if x < 0 && y < 0 {
+    print(3) // 제3사분면
+} else if x > 0 && y < 0 {
+    print(4) // 제4사분면
 }
-
-func poolBallType(forNumber number: Int) -> PoolBallType? {
-    if number < 8 && number > 0 {
-        return .solid
-    } else if number > 8 && number < 16 {
-        return .stripe
-    } else if number == 8 {
-        return .black
-    } else {
-        return nil
-    }
-}
-
-let two = poolBallType(forNumber: 2)
-let eight = poolBallType(forNumber: 8)
-let twelve = poolBallType(forNumber: 12)
-let zero = poolBallType(forNumber: 0)
-let sixteen = poolBallType(forNumber: 16)
 
